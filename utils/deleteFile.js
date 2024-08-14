2de1c1889c80507206f5ef16cf9fc08bfeb9b265
hossein <kianlucifer0098@gmail.com>
2024-08-14 11:20:04 +0330

    newUpdate

--- utils/deleteFile.js
+++ utils/deleteFile.js
@@ -1,6 +1,13 @@
 const OSS = require("ali-oss");
 
-// 
+const store = new OSS({
+  region: "oss-cn-shanghai",
+  accessKeyId: "LTAI5tSQooLRB9eQXncVQ8wr",
+  accessKeySecret: "nAUj9B0ov1CZn0I1DS6qRsP9uW26m2",
+  bucket: "ashoss",
+  secure: true,
+});
+
 exports.deleteFile = async (filePath) => {
   const path = filePath.split("ashoss.oss-cn-shanghai.aliyuncs.com")[1];
   await store
